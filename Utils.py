414f4a7c3a73651d2b8bdce850e99e7296346fdd
courtrightj7 <36347248+courtrightj7@users.noreply.github.com>
2018-02-11 13:27:34 -0500

    Update Utils.py

--- Utils.py
+++ Utils.py
@@ -4,6 +4,7 @@ def startConnection(database):
     password = 'Skynetcapital2017'
     driver = '{ODBC Driver 13 for SQL Server}'
     cnxn = pyodbc.connect('DRIVER=' + driver + ';PORT=1433;SERVER=' + server + ';PORT=1443;DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
+    print "TEST"
     return cnxn
 def closeConnection(conn):
   conn.close()
