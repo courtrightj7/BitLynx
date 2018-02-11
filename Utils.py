1f1565d912a0eb0f8c2a64e5cc9067dc081325f9
courtrightj7 <36347248+courtrightj7@users.noreply.github.com>
2018-02-11 12:27:00 -0500

    Create Utils.py
    Create Utils lib

---
+++ Utils.py
@@ -0,0 +1,9 @@
+def startConnection(database):
+    server = 'skynetcapital.database.windows.net'
+    username = 'amhardy94'
+    password = 'Skynetcapital2017'
+    driver = '{ODBC Driver 13 for SQL Server}'
+    cnxn = pyodbc.connect('DRIVER=' + driver + ';PORT=1433;SERVER=' + server + ';PORT=1443;DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
+    return cnxn
+def closeConnection(conn):
+  conn.close()
